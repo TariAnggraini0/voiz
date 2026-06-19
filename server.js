ba017e024b5debf1ba00b7fccaf98f70ec6f0cbe
TariAnggraini0 <tarianggraini937@gmail.com>
2026-06-19 13:04:17 +0700

    server.js

--- server.js
+++ server.js
@@ -3,15 +3,11 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const db = require('./config/db');
-
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
-
-// Serve frontend dari root repo
-app.use(express.static(path.join(__dirname)));
 
 // Routes API
 app.use('/api/auth', require('./routes/auth'));
@@ -19,16 +15,12 @@ app.use('/api/aspirasi', require('./routes/aspirasi'));
 app.use('/api/admin', require('./routes/admin'));
 app.use('/api/voting', require('./routes/voting'));
 
+// Serve semua file statis
+app.use(express.static(__dirname));
+
+// Fallback
 app.get('*', (req, res) => {
-  // Kalau ada ekstensi file (.html, .css, .js, dll), serve file itu
-  if (path.extname(req.path)) {
-    res.sendFile(path.join(__dirname, req.path), (err) => {
-      if (err) res.status(404).send('Not found');
-    });
-  } else {
-    // Kalau tidak ada ekstensi, fallback ke index.html
-    res.sendFile(path.join(__dirname, 'index.html'));
-  }
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(PORT, () => {
